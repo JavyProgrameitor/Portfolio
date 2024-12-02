5789c920cfa02d842241cd223dd8d09d80447cb6
javyprogrameitor <85842248+JavyProgrameitor@users.noreply.github.com>
2024-12-02 20:44:37 +0100

    New update
    We adapted the transitions of the cards

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let cards = document.querySelectorAll('.card');
-  
-    cards.forEach(card => {
-      card.addEventListener('mouseover', (e) => {
-        let positionX = window.innerWidth;
-        let positionY = window.innerHeight;
-  
-        card.style.transform = `rotateY(${(e.clientX / positionX - 0.8) * 25}deg) rotateX(${(e.clientY / positionY - 0.8) * 25}deg) scale(0.9)`;
-      });
-  
-      card.addEventListener('mouseout', () => {
-        card.style.transform = ''; 
-      });
+  let cards = document.querySelectorAll('.card');
+
+  cards.forEach(card => {
+    card.addEventListener('mousemove', (e) => {
+   
+      let positionX = window.innerWidth;
+      let positionY = window.innerHeight;
+
+   
+      let moveX = (e.clientX / positionX - 0.5) * 60; 
+      let moveY = (e.clientY / positionY - 0.5) * 60; 
+
+     
+      card.style.transform = `rotateY(${moveX}deg) rotateX(${moveY}deg)`;
+    });
+
+    card.addEventListener('mouseleave', () => {
+      // Reset the rotation on mouseleave
+      card.style.transform = '';
     });
   });
-  
+});
